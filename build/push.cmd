3d251da72637c40f01c8b6699ae0e16d1788e3e9
AdamMCraven <adam@ChannelAdam.com>
2016-11-04 02:36:04 +1000

    Updated nuget push command to specify the source to push to

--- build/push.cmd
+++ build/push.cmd
@@ -4,6 +4,6 @@ set packageName=ChannelAdam.Soap
 
 set /p version="What is the version you want to push?"
 
-..\tools\nuget\nuget.exe push "%packageName%.%version%.nupkg"
+..\tools\nuget\nuget.exe push "%packageName%.%version%.nupkg" -Source https://www.nuget.org/api/v2/package
 
 pause
